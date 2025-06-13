382ada903f83e0749cc88f96491e24b0a797e741
Aayush Pokhrel <114979385+1014Aayush@users.noreply.github.com>
2025-06-13 13:35:45 +0545

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h2>Live Hand Tracking</h2>
+      <img src="http://127.0.0.1:8000/video/" alt="Live Webcam Feed" width="640" />
+    </div>
+  );
+};
+
+export default App;
